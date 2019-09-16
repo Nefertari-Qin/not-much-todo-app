8863c3af9ccad4cc0d78345dc20aafe6a0e3e3fd
Nefertari-Qin <Nefertari-Qin@users.noreply.github.com>
2019-09-16 14:46:18 -0700

    Add some new classes.

---
+++ src/main/ui/ToDoApp.java
@@ -0,0 +1,36 @@
+package ui;
+
+public class ToDoApp {
+    public static void main(String[] args) {
+        simplePrint();
+        morePrint();
+        evenMorePrint();
+        kiddingPrint();
+        actualPrint();
+        endPrint();
+    }
+
+    public static void simplePrint() {
+        System.out.println("Just a very simple print ....");
+    }
+
+    public static void morePrint() {
+        System.out.println("Because I have gotten nothing in mind about my project yet.");
+    }
+
+    public static void evenMorePrint() {
+        System.out.println("So I just followed the simple description of Deliverable 1.");
+    }
+
+    public static void kiddingPrint() {
+        System.out.println("Hmmmm ...., just kidding! :)");
+    }
+
+    public static void actualPrint() {
+        System.out.println("I really do want to create a project somewhat like a ToDo list application!");
+    }
+
+    public static void endPrint() {
+        System.out.println("I know that doesn't sounds cool ...., but I think I can potentially use it!");
+    }
+}
