619a6c18d0dc5a677d3f72222cc883de56e080e0
Nefertari-Qin <Nefertari-Qin@users.noreply.github.com>
2019-10-31 08:50:44 -0700

    Update Test Class for Regular Task.

--- src/main/model/RegularTask.java
+++ src/main/model/RegularTask.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.util.Date;
+import java.util.Objects;
 
 public class RegularTask {
     private UrgencyLevel level;
@@ -10,23 +11,18 @@ public class RegularTask {
     private boolean isDone;
 
     public RegularTask(String content) {
+        level = UrgencyLevel.R;
         this.content = content;
+        isDue = false;
+        isDone = false;
     }
 
     public UrgencyLevel getLevel() {
         return level;
     }
 
-    public void setLevel(UrgencyLevel level) {
-        this.level = level;
-    }
-
     public String getContent() {
         return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
     }
 
     public Date getDueTime() {
@@ -51,5 +47,24 @@ public class RegularTask {
 
     public void setDone(boolean done) {
         isDone = done;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        RegularTask that = (RegularTask) o;
+        return isDue == that.isDue
+                && isDone == that.isDone && level == that.level
+                && content.equals(that.content);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(level, content, isDue, isDone);
     }
 }
